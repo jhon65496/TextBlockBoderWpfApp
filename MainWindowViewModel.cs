2d274f382076bef0d622a851d5b4fd2ee02c477d
jhon65496 <jhon65496@yandex.ru>
2023-09-25 12:32:39 +0300

    Добавьте файлы проекта.

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace TextBlockBoderWpfApp
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        private Phone selectedPhone;
+
+        public ObservableCollection<Phone> Phones { get; set; }
+
+        public Phone SelectedPhone
+        {
+            get { return selectedPhone; }
+            set
+            {
+                selectedPhone = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone { Title="iPhone 7", Company="Apple", Price=56000 },
+                new Phone {Title="Galaxy S7 Edge", Company="Samsung", Price =60000 },
+                new Phone {Title="Elite x3", Company="HP", Price=56000 },
+                new Phone {Title="Mi5S", Company="Xiaomi", Price=35000 }
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+    }
+}
